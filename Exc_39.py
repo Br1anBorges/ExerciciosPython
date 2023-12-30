ea69723cb3b05c6d031e730408241219658d0684
Br1anBorges <contatobrianborges@gmail.com>
2023-12-30 10:48:48 -0300

    Exercicio 39

--- Exc_39.py
+++ Exc_39.py
@@ -7,6 +7,29 @@ alguma este valor deve ser exibido
 import random
 
 valor = random.randint(0,1000)
-resto = valor 
-if resto == 0:
-    print (f'O número {valor} é divisivel por 2')
+resto= valor%10
+
+while valor != 0 or valor != 2 or valor != 4 or valor != 6 or valor != 8:
+    auxiliar=0
+    resto = valor%10
+    valor= valor/10
+    auxiliar= valor+resto+auxiliar
+    if auxiliar == 0 or auxiliar == 2 or auxiliar == 4 or auxiliar ==6 or auxiliar == 8:
+        print (f'Valor é divisivel por 3')
+        break
+    match resto:
+        case 0: 
+            print (f'É divisivel por 2')
+            break
+        case 2: 
+            print(f'É divisivel por 2')
+            break
+        case 4: 
+            print (f'É divisivel por 2')
+            break
+        case 6:
+            print (f'É divisivel por 2')
+            break
+        case 8:
+            print (f'É divisivel por 2')
+            break
