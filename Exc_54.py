fabc740fbff690edb4e0f4a56a2abaa51edbec60
Br1anBorges <contatobrianborges@gmail.com>
2024-01-05 20:23:33 -0300

    Exercicio 54

---
+++ Exc_54.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que imprima na tela apenas os números
+ímpares entre 1 e 50.
+'''
+
+
+lista_Numeros=[]
+
+for numero in range(1,50,2):
+    print(numero)
+    lista_Numeros.append(numero)
